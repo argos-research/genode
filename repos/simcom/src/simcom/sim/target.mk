10ad442063c4ea0b2036ade9b1941e1e80bfb9f9
Georg Guba <georgguba@gmail.com>
2015-12-08 17:20:27 +0100

    Makefile for panda build and repos from IDP added

---
+++ repos/simcom/src/simcom/sim/target.mk
@@ -0,0 +1,9 @@
+TARGET = simcom_sim
+SRC_CC = main.cc
+LIBS   = base lwip libc stdcxx
+REQUIRES = foc
+
+#LWIP_PORT_DIR := $(call select_from_ports,lwip)
+#LWIP_DIR      := $(LWIP_PORT_DIR)/src/lib/lwip
+
+INC_DIR += $(REP_DIR)/../libports/include/lwip

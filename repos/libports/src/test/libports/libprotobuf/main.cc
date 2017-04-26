3e1d50381644768c7c3c4df9e2701459dace7140
Alexander Weidinger <alexwegoo@gmail.com>
2017-04-26 22:37:59 +0200

    Cleanup.

--- repos/libports/src/test/libports/libprotobuf/main.cc
+++ repos/libports/src/test/libports/libprotobuf/main.cc
@@ -1,15 +1,4 @@
-/*
- * \brief  Link test
- * \author Norman Feske
- * \date   2009-10-16
- */
-
-/*
- * Copyright (C) 2009-2013 Genode Labs GmbH
- *
- * This file is part of the Genode OS framework, which is distributed
- * under the terms of the GNU General Public License version 2.
- */
+/* just a simple test file to confirm linking */
 
 #include "vehicle.pb.h"
 
